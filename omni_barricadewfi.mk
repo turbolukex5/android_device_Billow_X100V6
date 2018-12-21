05fa696dd170d2eb0bd2631e6afbe96059d60879
turbolukex5 <42676704+turbolukex5@users.noreply.github.com>
2018-12-21 16:13:44 +0000

    Update and rename omni_X100V6.mk to omni_barricadewfi.mk

---
+++ omni_barricadewfi.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/barricadewifi/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := barricadewifi
+PRODUCT_NAME := omni_barricadewifi
+PRODUCT_BRAND := Acer
+PRODUCT_MANUFACTURER := Acer
+PRODUCT_MODEL := Acer B1-790
